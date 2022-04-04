330eaa6c80729d7652a88a59d71762991db6cde9
pkcarlos <patricia.k.carlos@gmail.com>
2022-04-03 18:59:14 -0700

    update files

---
+++ exercises/RB101-RB109/Easy3/palindromic_strings.rb
@@ -0,0 +1,24 @@
+# Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. A palindrome reads the same forward and backward. For this exercise, case matters as does punctuation and spaces.
+
+def palindrome?(str)
+  return true if str.reverse == str
+  false
+end
+
+p palindrome?('madam') == true
+p palindrome?('Madam') == false   # (case matters)
+p palindrome?("madam i'm adam") == false # (all characters matter)
+p palindrome?('356653') == true
+
+# for an array
+def palindrome?(arr)
+  arr == arr.reverse
+end
+
+# for either string or array
+def palindrome?(obj)
+  obj == obj.reverse
+end
+
+
+  
